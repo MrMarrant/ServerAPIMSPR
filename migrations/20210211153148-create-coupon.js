76e2934328c33caca7946ca5e49c4b8dd79e8663
Lucas Reteau <lucaporal@live.fr>
2021-02-11 16:56:00 +0100

    creating coupon

---
+++ migrations/20210211153148-create-coupon.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Coupons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      dateDebut: {
+        type: Sequelize.DATE
+      },
+      dateExpiration: {
+        type: Sequelize.DATE
+      },
+      productId: {
+        type: Sequelize.INTEGER
+      },
+      reduction: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Coupons');
+  }
+};
