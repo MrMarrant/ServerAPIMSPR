19e336bbeb21970f275f7f9ce40910dc8c57af53
MrMarrant <georgesandalous@gmail.com>
2021-03-18 17:42:02 +0100

    creation model association coupon User

---
+++ migrations/20210318140224-create-users-coupons.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UsersCoupons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        
+      },
+      couponId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UsersCoupons');
+  }
+};
